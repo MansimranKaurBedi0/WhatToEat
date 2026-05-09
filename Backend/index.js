2c072722249c2f7fb8b624862c11cd87d2662636
MANSIMRAN <mansimran1414@gmail.com>
2026-05-09 22:36:10 +0530

    MongoDb connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+import connectDb from './config/db.js';
 const app = express();
+connectDb();
 app.listen(3000,()=>{
   console.log("Server is running on port 3000");
-})
+});
