6a593e3e780c1cb483d908517364bfbbf293d2cf
MANSIMRAN <mansimran1414@gmail.com>
2026-05-10 00:42:17 +0530

    Login Route and SignUp Route added

---
+++ Backend/routes/auth.js
@@ -0,0 +1,6 @@
+import { login,signup } from "../controller/auth.controller";
+import express from "express";
+const router=express.Router();
+router.post('/login',login);
+router.post('/signup',signup);
+export default router;
