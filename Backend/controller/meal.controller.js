07d9d075ca4cf48c6cbede57de1b66117e318583
MANSIMRAN <mansimran1414@gmail.com>
2026-05-19 00:14:36 +0530

    Controller for getting random mean based on your health

---
+++ Backend/controller/meal.controller.js
@@ -0,0 +1,114 @@
+import User from "../models/Users.js";
+import FoodLog from "../models/FoodLog.js";
+
+import {
+    calculateHealthMetrics
+} from "../services/health.js";
+
+import {
+    analyzeHealthWithAI
+} from "../services/ai.services.js";
+
+import {
+    getRandomMealSuggestions
+} from "../services/randomRecommendation.service.js";
+
+
+export const recommendMeals =
+    async (req, res) => {
+
+        try {
+
+            const userId =
+                req.user.id;
+
+
+            // user fetch
+            const user =
+                await User.findById(userId);
+
+            if (!user) {
+                return res.status(404).json({
+                    message: "User not found"
+                });
+            }
+
+
+            // food logs fetch
+            const foodLogs =
+                await FoodLog.find({
+                    userId
+                });
+
+
+            if (foodLogs.length === 0) {
+                return res.status(400).json({
+                    message:
+                        "No food history found. Track food first."
+                });
+            }
+
+
+            // metrics generate
+            const metrics =
+                calculateHealthMetrics(
+                    foodLogs, user
+                );
+
+
+            // AI health insights
+            const healthInsights =
+                await analyzeHealthWithAI(
+                    user,
+                    metrics
+                );
+
+
+            // AI meal recommendation
+            const meals =
+                await getRandomMealSuggestions(
+                    user,
+                    metrics,
+                    healthInsights
+                );
+
+
+            return res.status(200).json({
+                success: true,
+                meals
+            });
+
+        }
+
+        catch (error) {
+
+            console.log(
+                "========== MEAL API ERROR =========="
+            );
+
+            console.log(
+                "Error Message:",
+                error.message
+            );
+
+            console.log(
+                "Full Error:",
+                error
+            );
+
+            console.log(
+                "Stack Trace:",
+                error.stack
+            );
+
+            return res.status(500).json({
+                success: false,
+                actualError:
+                    error.message,
+                fullError:
+                    error.toString()
+            });
+
+        }
+
+    };
